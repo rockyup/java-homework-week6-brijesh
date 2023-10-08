4bed36ef218664da51231dceee2a71c0bfb82da9
rockyup <rockyup009@gmail.com>
2023-10-08 16:34:05 +0100

    My homework week 6

---
+++ src/homeworkweek6/BioData.java
@@ -0,0 +1,13 @@
+package homeworkweek6;
+
+public class BioData {
+    public static void main(String [] args){
+        String title = "BioData";
+        String name = "Name : Rocky Verma";
+        String age = "age :35";
+        System.out.println(title);
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
+
---
+++ src/homeworkweek6/Triangle1.java
@@ -0,0 +1,17 @@
+package homeworkweek6;
+
+public class Triangle1 {
+        public static void main(String [] args){
+//this is how look like right triangle
+            System.out.println("*");
+            System.out.println("** ");
+            System.out.println("*** ");
+            System.out.println("**** ");
+            System.out.println("***** ");
+            System.out.println("****** ");
+            System.out.println("******* ");
+
+
+        }
+
+}
